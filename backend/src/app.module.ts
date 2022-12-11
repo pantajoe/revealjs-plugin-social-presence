a8acbba1f21b3ab13fbc5dbd45b45c824a73a0bd
Joe Pantazidis <joe.pantazidis@outlook.de>
2022-12-11 10:46:32 +0100

    chore: add demo as static files to backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,6 @@
+import { join } from 'path'
 import { Module } from '@nestjs/common'
+import { ServeStaticModule } from '@nestjs/serve-static'
 import { CoreModule } from './core/core.module'
 import { StorageModule } from './storage/storage.module'
 import { HealthModule } from './health/health.module'
@@ -7,6 +9,21 @@ import { ChatModule } from './chat/chat.module'
 import { AnnotationModule } from './annotation/annotation.module'
 
 @Module({
-  imports: [CoreModule, HealthModule, StorageModule, PresenceModule, ChatModule, AnnotationModule],
+  imports: [
+    CoreModule,
+    HealthModule,
+    StorageModule,
+    PresenceModule,
+    ChatModule,
+    AnnotationModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'public'),
+      serveStaticOptions: {
+        cacheControl: true,
+        immutable: true,
+        maxAge: 31536000,
+      },
+    }),
+  ],
 })
 export class AppModule {}
