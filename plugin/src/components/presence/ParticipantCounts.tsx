775dccf9a8a5165a6b2b4eb80ac8b48ad1f784d0
Joe Pantazidis <joe.pantazidis@outlook.de>
2022-12-15 07:16:33 +0100

    fix: fix participant counts on overview slides

--- plugin/src/components/presence/ParticipantCounts.tsx
+++ plugin/src/components/presence/ParticipantCounts.tsx
@@ -5,7 +5,7 @@ const createSlidePresenceElement = (presence: number) => {
   const element = document.createElement('span')
   element.className =
     'slide-presence-element hidden absolute left-4 bottom-4 rounded-full text-white bg-red-400 font-semibold inline-flex items-center justify-center text-7xl w-40 h-40'
-  element.textContent = `+${presence + 1}`
+  element.textContent = `+${presence}`
   return element
 }
 
